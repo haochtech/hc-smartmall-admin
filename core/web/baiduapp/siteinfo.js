d47b97c3a09e922e86793a04ec86830d8b4a2236
hek <yeyu0424@163.com>
2020-05-15 14:26:29 +0800

    添加项目文件

---
+++ core/web/baiduapp/siteinfo.js
@@ -0,0 +1,12 @@
+
+var siteinfo = {
+    name: "智能小程序",
+    uniacid: "961",
+    acid: "961",
+    multiid: "0",
+    version: "2.7.6",
+    siteroot: "https://zx-app.cn/app/index.php",
+    design_method: "3"
+};
+
+module.exports = siteinfo;
---
+++ wxapp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @link http://www.zx-xcx.com/
+ * @copyright Copyright (c) 2018 智享工场
+ * @author Lu Wei
+ *
+ * Created by IntelliJ IDEA.
+ * User: wane_x
+ * Date: 2019/3/23
+ * Time: 16:27
+ */
